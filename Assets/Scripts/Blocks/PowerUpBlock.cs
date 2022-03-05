8a3d60a167c130a03f3409aae5128bb53a2879d4
Jack Coggins <voidawake90@gmail.com>
2022-03-05 21:54:44 +1100

    Powerup block

---
+++ Assets/Scripts/Blocks/PowerUpBlock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Blocks
+{
+    public class PowerUpBlock : Block
+    {
+        [SerializeField] private Sprite triggeredSprite;
+        [SerializeField] private GameObject blockPowerUpPrefab;
+
+        private SpriteRenderer spriteRenderer;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        [ContextMenu("Trigger")]
+        protected override void Trigger()
+        {
+            spriteRenderer.sprite = triggeredSprite;
+            
+            Bump();
+        }
+
+        protected override void OnBumpComplete()
+        {
+            base.OnBumpComplete();
+
+            Instantiate(blockPowerUpPrefab, transform);
+
+            enabled = false;
+        }
+    }
+}
