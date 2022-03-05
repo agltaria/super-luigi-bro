c04a3f16435049cf937539b359f7da91bf4fa9ba
Jack Coggins <voidawake90@gmail.com>
2022-03-05 21:53:44 +1100

    Coin block

---
+++ Assets/Scripts/Blocks/CoinBlock.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Blocks
+{
+    public class CoinBlock : Block
+    {
+        [SerializeField] private Sprite triggeredSprite;
+        [SerializeField] private int numberOfCoins;
+        [SerializeField] private GameObject blockCoinPrefab;
+        [SerializeField] private Vector2 spawnOffset;
+
+        private SpriteRenderer spriteRenderer;
+        private int coinsSpawned;
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        }
+
+        [ContextMenu("Trigger")]
+        protected override void Trigger()
+        {
+            if (coinsSpawned >= numberOfCoins - 1)
+                spriteRenderer.sprite = triggeredSprite;
+            
+            Instantiate(blockCoinPrefab, transform.position + (Vector3) spawnOffset, Quaternion.identity, transform);
+            
+            coinsSpawned++;
+            
+            Bump();
+        }
+
+        protected override void OnBumpComplete()
+        {
+            base.OnBumpComplete();
+            
+            // TODO: Update coin count
+            
+            if (coinsSpawned >= numberOfCoins)
+                enabled = false;
+        }
+    }
+}
