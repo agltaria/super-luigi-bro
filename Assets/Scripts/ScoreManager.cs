83c64e3295423761fa19b1de68945aaa5b5a33e7
christa-zhang-uts <13926086@student.uts.edu.au>
2022-03-09 01:08:17 +1100

    Score hooked up (hopefully)

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int score;
+    public static int score = 0;
+
+    public UnityEvent scoreChanged = new UnityEvent();
+
+    private float _currentScore;
+
+    public float currentScore
+    {
+        get => _currentScore;
+        private set
+        {
+            _currentScore = value;
+            scoreChanged.Invoke();
+        }
+    }
+
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -15,5 +32,15 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
     }
 }
