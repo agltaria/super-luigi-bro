8594dc6c4ace1e5c522f4379c57a2146f4f07864
christa-zhang-uts <13926086@student.uts.edu.au>
2022-03-08 23:13:31 +1100

    UI layout

--- Assets/Scripts/UI/CoinsUI.cs
+++ Assets/Scripts/UI/CoinsUI.cs
@@ -1,21 +1,25 @@
 ﻿using DefaultNamespace;
-using TMPro;
+//using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 namespace UI
 {
-    [RequireComponent(typeof(TextMeshProUGUI))]
+    //[RequireComponent(typeof(TextMeshProUGUI))]
+    [RequireComponent(typeof(Text))]
     public class CoinsUI : MonoBehaviour
     {
         [SerializeField] private CoinManager coinManager;
 
-        private TextMeshProUGUI text;
+        //private TextMeshProUGUI text;
+        private Text text;
 
         private void Awake()
         {
             coinManager.coinsChanged.AddListener(OnCoinsChanged);
 
-            text = GetComponent<TextMeshProUGUI>();
+            //text = GetComponent<TextMeshProUGUI>();
+            text = GetComponent<Text>();
         }
 
         private void OnCoinsChanged()
