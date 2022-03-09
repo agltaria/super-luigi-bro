0eef9a5c5eeab2da425f1e8a4e9bd9248efab158
christa-zhang-uts <13926086@student.uts.edu.au>
2022-03-09 23:40:26 +1100

    Added Star Theme + Oneshots

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -26,4 +26,69 @@ public class AudioManager : MonoBehaviour
     {
         
     }
+
+    public void playSmallJump()
+    {
+        audioPlayer.PlayOneShot(clips[0]);
+    }
+
+    public void playSuperJump()
+    {
+        audioPlayer.PlayOneShot(clips[1]);
+    }
+
+    public void playCoin()
+    {
+        audioPlayer.PlayOneShot(clips[2]);
+    }
+
+    public void playBreakBlock()
+    {
+        audioPlayer.PlayOneShot(clips[3]);
+    }
+
+    public void playBump()
+    {
+        audioPlayer.PlayOneShot(clips[4]);
+    }
+
+    public void playPowerAppear()
+    {
+        audioPlayer.PlayOneShot(clips[5]);
+    }
+
+    public void playPowerup()
+    {
+        audioPlayer.PlayOneShot(clips[6]);
+    }
+
+    public void play1Up()
+    {
+        audioPlayer.PlayOneShot(clips[7]);
+    }
+
+    public void playStomp()
+    {
+        audioPlayer.PlayOneShot(clips[8]);
+    }
+
+    public void playFireball()
+    {
+        audioPlayer.PlayOneShot(clips[9]);
+    }
+
+    public void playPipe()
+    {
+        audioPlayer.PlayOneShot(clips[10]);
+    }
+
+    public void playFlag()
+    {
+        audioPlayer.PlayOneShot(clips[11]);
+    }
+
+    public void playDie()
+    {
+        audioPlayer.PlayOneShot(clips[13]);
+    }
 }
