c3a11b819f04d1a0aef385f03d7907df65b58b66
Jack Coggins <voidawake90@gmail.com>
2022-03-09 22:47:17 +1100

    Death zone under stage

---
+++ Assets/Scripts/DeathZone.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class DeathZone : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (!col.CompareTag("Player")) return;
+            
+            PlayerDeath.Instance.Die();
+        }
+    }
+}
